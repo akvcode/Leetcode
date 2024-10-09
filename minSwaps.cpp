04ab6e491ada8728a9d189b6cf51be7bae23270e
Akash Vishwakarma <skytup91@gmail.com>
2024-10-10 01:53:42 +0530

    Updating the minSwaps file

--- minSwaps.cpp
+++ minSwaps.cpp
@@ -3,46 +3,52 @@
 #include <string>
 
 using namespace std;
-
+// This code is written by Akash Vishwakarma on leetcode 10/10/2024
 // Solution class
-class Solution {
+class Solution
+{
 public:
-    int minSwaps(string s) {
+    int minSwaps(string s)
+    {
         // Initialize a stack to keep track of opening brackets
         stack<char> st;
-        
+
         // Iterate over each character in the string
-        for(char &ch: s){
+        for (char &ch : s)
+        {
             // If the character is an opening bracket, push it onto the stack
-            if(ch=='['){
+            if (ch == '[')
+            {
                 st.push(ch);
-            } 
-            // If the character is a closing bracket and the stack is not empty, 
+            }
+            // If the character is a closing bracket and the stack is not empty,
             // pop the corresponding opening bracket from the stack
-            else if(!st.empty()){
+            else if (!st.empty())
+            {
                 st.pop();
             }
         }
-        
+
         // The minimum number of swaps required is half the number of unmatched opening brackets plus one
         // This is because each swap can balance two brackets
-        return (st.size()+1)/2;
+        return (st.size() + 1) / 2;
     }
 };
 
-int main() {
+int main()
+{
     Solution solution;
     string s;
-    
+
     // Input string
     cout << "Enter bracket sequence: ";
     cin >> s;
-    
+
     // Calculate minimum swaps
     int minSwapsRequired = solution.minSwaps(s);
-    
+
     // Display result
     cout << "Minimum Swaps Required: " << minSwapsRequired << endl;
-    
+
     return 0;
 }
