21a7c9796b381eeb3cb01a6b27f02d95ba2ef551
Akash <skytup91@gmail.com>
2025-01-10 22:56:21 +0530

    Finally Solved after 4 hour 😂Kya main Engineer ban paunga😅 ?

--- word-subsets/sky.cpp
+++ word-subsets/sky.cpp
@@ -1,17 +1,15 @@
 // Akash Vishwakarma
 // Date - 10/01/2025
 // www.skytup.com
-// This question has taken approx 4 hour to solve for me 😂😂😂 But at the end, I just solved it 
+// This question has taken approx 4 hour to solve for me 😂😂😂 But at the end I just solved it 
 
 #include <bits/stdc++.h>
 #include <vector>
 #include <string>
 using namespace std;
-
 class Solution
 {
 public:
-    // Function to return maximum character frequency map
     unordered_map<char, int> returnIdentity(vector<string> &str)
     {
         unordered_map<char, int> charFrequency;
@@ -36,7 +34,6 @@ public:
     vector<string> wordSubsets(vector<string> &words1, vector<string> &words2)
     {
         vector<string> ans;
-        // Get the target character frequencies from words2
         auto targetFrequency = returnIdentity(words2);
 
         for (const auto &word : words1)
@@ -45,18 +42,16 @@ public:
             vector<string> ch;
             ch.push_back(word);
             auto num = returnIdentity(ch);
-            ch.clear(); // Fix: Use clear() instead of empty()
+            ch.clear(); 
 
             for (auto &x : targetFrequency)
             {
-                // Check if the word satisfies character frequency requirements
                 if (word.find(x.first) < word.size() && num[x.first] >= x.second)
                 {
                     temp++;
                 }
             }
 
-            // If all conditions are satisfied, add to result
             if (temp == targetFrequency.size())
             {
                 ans.push_back(word);
@@ -68,18 +63,13 @@ public:
 
 int main()
 {
-    // Sample input
+    // vector<string> a= {"Rahul","Akash","Shivam","Abhay"};
     vector<string> a = {"amazon", "apple", "facebook", "google", "leetcode"};
     vector<string> b = {"e", "oo"};
-
     Solution s;
     vector<string> result = s.wordSubsets(a, b);
-
-    // Output the result
     for (auto &x : result)
     {
         cout << x << endl;
     }
-
-    return 0;
 }
