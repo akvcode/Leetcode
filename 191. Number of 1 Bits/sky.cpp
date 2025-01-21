964ed79de731d26bb8ee5d612ff7e4de1e409d4f
Akash <skytup91@gmail.com>
2025-01-22 04:16:14 +0530

    adding new prblem

---
+++ 191. Number of 1 Bits/sky.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int hammingWeight(int n) {
+        int count =0;
+        while (n!=0)
+        {
+            // checking last bit
+            if(n&1){
+                count++;
+            }
+            n>>=1;
+        }
+        return count;
+    }
+};
+int main (){
+    Solution s;
+    cout << s.hammingWeight(5);
+}
